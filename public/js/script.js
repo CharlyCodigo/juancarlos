689423958ecda7ae8b031a8648843c50e235adba
CharlyCodigo <juancarlos45836@gmail.com>
2023-05-08 16:48:31 -0600

    validacion de campos vacios de formulario

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,8 @@ const telefono = document.getElementById("telefono");
 const email = document.getElementById("email");
 const asunto = document.getElementById("asunto");
 const contenido = document.getElementById("contenido");
-const confirmacion =document.getElementById("confirmacion");
+const confirmacion = document.getElementById("confirmacion");
+const msgenviado = document.getElementById("msgenviado");
 const icon = document.getElementById("icon");
 const listMenu = document.getElementById("list-menu");
 const menuInicio = document.getElementById("menu-inicio");
@@ -11,9 +12,52 @@ const menuSobremi = document.getElementById("menu-sobremi");
 const menuSkills = document.getElementById("menu-skills");
 const menuMitrabajo = document.getElementById("menu-mitrabajo");
 const menuContacto = document.getElementById("menu-contacto");
-const buttonSubmit = document.getElementsByClassName("boton");
+const buttonSubmit = document.getElementById("boton");
+const formulario =  document.getElementById("formulario");
 
+let visitas = 0;
+confirmacion.setAttribute("style","display:none;")
+function submit() {
+    
+    if(nombre.value === "" || telefono.value === "" || email.value === "" || asunto.value === "" || contenido.value === ""){
+        nombre.value ==="" ? nombre.setAttribute('style', 'border-color:red;') : nombre.setAttribute('style', 'border-color:white;');
+        telefono.value == "" ? telefono.setAttribute('style', 'border-color:red;') : telefono.setAttribute('style', 'border-color:white;');
+        email.value === "" ? email.setAttribute('style', 'border-color:red;') : email.setAttribute('style', 'border-color:white;');
+        asunto.value === "" ? asunto.setAttribute('style', 'border-color:red;') : asunto.setAttribute('style', 'border-color:white;');
+        contenido.value === "" ? contenido.setAttribute('style', 'border-color:red;') : contenido.setAttribute('style', 'border-color:white;');
+        confirmacion.setAttribute("style","display:inherit;")
+    }else {
+        formulario.setAttribute("onsubmit","return true");
+        formulario.submit()
+    }
+}
 
+function nombreMargin() {
+    nombre.setAttribute('style', 'border-color:white;');
+    confirmacion.setAttribute('style','display:none;');
+}
+function telefonoMargin() {
+    telefono.setAttribute('style', 'border-color:white;')
+    confirmacion.setAttribute('style','display:none;');
+}
+function emailMargin() {
+    email.setAttribute('style', 'border-color:white;')
+    confirmacion.setAttribute('style','display:none;');
+}
+function asuntoMargin() {
+    asunto.setAttribute('style', 'border-color:white;')
+    confirmacion.setAttribute('style','display:none;');
+}
+function contenidoMargin() {
+    contenido.setAttribute('style', 'border-color:white;')
+    confirmacion.setAttribute('style','display:none;');
+}
+buttonSubmit.addEventListener('click',()=>submit());
+nombre.addEventListener('change', () => nombreMargin());
+telefono.addEventListener('change', () => telefonoMargin());
+email.addEventListener('change', () => emailMargin());
+asunto.addEventListener('change', () => asuntoMargin());
+contenido.addEventListener('change', () => contenidoMargin());
 
 icon.setAttribute("style", "font-size:30px;")
 let visible = false;
@@ -26,8 +70,8 @@ function limpiarFormulario() {
     asunto.value="";
     contenido.value="";
     const text = "Mensaje Enviado";
-    confirmacion.setAttribute("style","color:white; font-size:20px;")
-    confirmacion.append(text);
+    msgenviado.setAttribute("style","color:white; font-size:20px;")
+    msgenviado.append(text);
     
 }
 
@@ -54,4 +98,4 @@ menuMitrabajo.addEventListener("click",  () => clickMenuSection());
 menuContacto.addEventListener("click",  () => clickMenuSection());
 
 window.onbeforeunload = limpiarFormulario;
-window.scrollY = clickMenuSection()
+
