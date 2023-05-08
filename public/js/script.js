0691973636ef890080f874dde9718f19b79c6a18
CharlyCodigo <juancarlos45836@gmail.com>
2023-05-08 13:23:05 -0600

    estilos mensaje de enviado y boton del menu en responsivo

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,9 @@
+const nombre = document.getElementById("nombre");
+const telefono = document.getElementById("telefono");
+const email = document.getElementById("email");
+const asunto = document.getElementById("asunto");
+const contenido = document.getElementById("contenido");
+const confirmacion =document.getElementById("confirmacion");
 const icon = document.getElementById("icon");
 const listMenu = document.getElementById("list-menu");
 const menuInicio = document.getElementById("menu-inicio");
@@ -5,14 +11,30 @@ const menuSobremi = document.getElementById("menu-sobremi");
 const menuSkills = document.getElementById("menu-skills");
 const menuMitrabajo = document.getElementById("menu-mitrabajo");
 const menuContacto = document.getElementById("menu-contacto");
+const buttonSubmit = document.getElementsByClassName("boton");
 
+
+
+icon.setAttribute("style", "font-size:30px;")
 let visible = false;
 listMenu.setAttribute("style","display:none;");
+
+function limpiarFormulario() {
+    nombre.value="";
+    telefono.value="";
+    email.value="";
+    asunto.value="";
+    contenido.value="";
+    const text = "Mensaje Enviado";
+    confirmacion.setAttribute("style","color:white; font-size:20px;")
+    confirmacion.append(text);
+    
+}
 
 function clickIcon() {
     if(visible === false) {
         visible = true;
-        listMenu.setAttribute("style","display:inherit;"); 
+        listMenu.setAttribute("style","display:inherit; padding:20px"); 
     } else {
         listMenu.setAttribute("style","display:none;");
         visible = false;
@@ -31,3 +53,5 @@ menuSkills.addEventListener("click",  () => clickMenuSection());
 menuMitrabajo.addEventListener("click",  () => clickMenuSection());
 menuContacto.addEventListener("click",  () => clickMenuSection());
 
+window.onbeforeunload = limpiarFormulario;
+window.scrollY = clickMenuSection()
